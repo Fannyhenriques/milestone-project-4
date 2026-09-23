000e8bc0c787ddc13509df9018465702984e4fff
iamfannyhenriques <fanny.henriques@hotmail.com>
2026-09-23 21:54:34 +0200

    Add Package model with items and pricing

--- services/models.py
+++ services/models.py
@@ -23,3 +23,17 @@ class Treatment(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Package(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    included = models.TextField()
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
