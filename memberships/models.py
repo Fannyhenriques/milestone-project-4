fbcdb8dd3e858855cce7b93286278c8491eb9bd6
iamfannyhenriques <fanny.henriques@hotmail.com>
2026-09-25 17:49:08 +0200

    Create Membership model

--- memberships/models.py
+++ memberships/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Membership(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="membership",
+    )
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.username} membership"
